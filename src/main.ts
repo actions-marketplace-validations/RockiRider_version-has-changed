d12d62f86bab92f918653b8accf962ecdf4596ff
Tsotne Gvadzabia <tsotne24@hotmail.com>
2023-06-22 17:06:58 +0100

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,37 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import {getInput, debug, setFailed, setOutput} from '@actions/core'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const workspace = getInput('workspace', {required: true})
+    const from = getInput('from', {required: true})
+    const to = getInput('to', {required: true})
+    const workingDirectory = getInput('working-directory', {required: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    debug(`Inputs: ${JSON.stringify({workspace, from, to, workingDirectory})}`)
 
-    core.setOutput('time', new Date().toTimeString())
+    let output = ''
+    let errors = ''
+
+    const options = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          output += data.toString()
+        },
+        stderr: (data: Buffer) => {
+          errors += data.toString()
+        }
+      },
+      cwd: './lib'
+    }
+    await exec.exec(`git diff ${from}..${to}`, [], options)
+
+    debug(`Output from Git Diff: ${output}`)
+    if (errors.length > 0) {
+      setFailed(errors)
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) setFailed(error.message)
   }
 }
 
